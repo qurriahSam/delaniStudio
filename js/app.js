f86b41a32112c73f23f66ed7049d37ce26f7be5f
90150N <mugirethe1@gmail.com>
2022-03-14 15:52:23 +0300

    ft: add name check function

--- js/app.js
+++ js/app.js
@@ -19,9 +19,29 @@ $(document).ready(function () {
   });
 
   portfolio.map((elem) => {
-    $(`#${elem}Container`).click(function () {
+    $(`#${elem}Container`).hover(function () {
       $(`#${elem}Bg`).toggleClass("d-none");
       $(`#${elem}Text`).fadeToggle();
     });
+  });
+
+  const nameCheck = (user) => {
+    let testReg = /[a-z ]/i;
+    let result = true;
+    user.split("").map((letter) => {
+      if (!testReg.test(letter)) {
+        result = false;
+      }
+    });
+    return result;
+  };
+
+  $("#btn").click(function (e) {
+    e.preventDefault();
+    let name = $("#name").val();
+
+    if (!nameCheck(name) || name.length <= 2) {
+      alert("enter valid name");
+    }
   });
 });
