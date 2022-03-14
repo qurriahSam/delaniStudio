aa931492e77a49584f864927bbf3b1cf79070231
90150N <mugirethe1@gmail.com>
2022-03-14 10:04:05 +0300

    ft: display portfolio text on click

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
   let whatWeDo = ["design", "development", "product"];
+  let portfolio = [
+    "projectOne",
+    "projectTwo",
+    "projectThree",
+    "projectFour",
+    "projectFive",
+    "projectSix",
+    "projectSeven",
+    "projectEight",
+  ];
+
   whatWeDo.map((elem) => {
     $(`#${elem}`).click(function () {
       $(`#${elem}Text`).toggleClass("d-none");
       $(`#${elem}Img`).toggleClass("d-none");
+    });
+  });
+
+  portfolio.map((elem) => {
+    $(`#${elem}Container`).click(function () {
+      $(`#${elem}Bg`).toggleClass("d-none");
+      $(`#${elem}Text`).fadeToggle();
     });
   });
 });
